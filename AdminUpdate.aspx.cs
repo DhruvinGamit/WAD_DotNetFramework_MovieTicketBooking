78990dc40456b870038726de5a5ee5aa1348fbcf
DhruvinGamit <dhruving21@gmail.com>
2023-10-24 19:26:59 +0530

    comitted

---
+++ AdminUpdate.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace Movie_Ticket_Booking
+{
+    public partial class AdminUpdate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Initialization logic here
+        }
+
+        protected void btnUpdateMovie_Click(object sender, EventArgs e)
+        {
+            // Retrieve values from textboxes
+            string movieName = txtUpdateMovieName.Text;
+            string newReleaseDate = txtNewReleaseDate.Text;
+            decimal newPrice = Convert.ToDecimal(txtNewPrice.Text);
+            TimeSpan newShowTime = TimeSpan.Parse(txtNewShowTime.Text);
+
+            // Replace the connection string with your actual database connection string
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\dhruv\\Desktop\\WAD_DotNetFramework_MovieTicketBooking_CE039_CE053\\Movie Ticket Booking\\Data\\Movie.mdf\";Integrated Security=True;Connect Timeout=30";
+
+            // Replace the SQL query with your actual update query
+            string query = "UPDATE Movies SET ReleaseDate = @NewReleaseDate, Price = @NewPrice, showTime = @NewShowTime WHERE MovieName = @MovieName";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Add parameters to prevent SQL injection
+                    command.Parameters.AddWithValue("@MovieName", movieName);
+                    command.Parameters.AddWithValue("@NewReleaseDate", newReleaseDate);
+                    command.Parameters.AddWithValue("@NewPrice", newPrice);
+                    command.Parameters.AddWithValue("@NewShowTime", newShowTime);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "script", "alert('Movie Updated successfully!');");
+                        ClearControls();
+                    }
+                    else
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "script", "alert('Movie not found or update failed.');");
+                    }
+                }
+            }
+        }
+
+        void ClearControls()
+        {
+            txtUpdateMovieName.Text = txtNewReleaseDate.Text = txtNewPrice.Text = txtNewShowTime.Text = "";
+        }
+    }
+}
---
+++ Confirmed.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Confirmed.aspx.cs" Inherits="Movie_Ticket_Booking.Confirmed" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        /* Add your styles here */
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <!-- Your HTML content here -->
+
+            <center>
+                <h2><b>Tickets have been successfully booked!</b></h2>
+                <br />
+            </center>
+            <br />
+            <center>
+                <h2><u>BOOKING DETAILS :</u></h2>
+            </center>
+            <center>
+                <h2>
+                    <asp:Label ID="lblMovieName" runat="server" Text=""></asp:Label>
+                </h2>
+                <h2>
+                    <asp:Label ID="lblRow" runat="server" Text=""></asp:Label>
+                    <asp:Label ID="lblSeat" runat="server" Text=""></asp:Label>
+                </h2>
+                <p>&nbsp;</p>
+            </center>
+            <br />
+            <center>
+                <h2><b>AMOUNT TO BE PAID : <asp:Label ID="lblAmount" runat="server"></asp:Label></b></h2>
+                PAYMENT METHOD : PAID
+                <br />
+                <br />
+                <br />
+            </center>
+        </div>
+    </form>
+</body>
+</html>
